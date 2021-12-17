b8638eb5c3f22a09409bb686ccd75030804e6ac3
Tatschfer <fernandaamengual@yahoo.com.br>
2021-12-16 22:57:48 -0300

    primeira-versao

---
+++ README.md
@@ -0,0 +1,7 @@
+# Quiz "Você conhece os personagens de The Office pelo sobrenome?"
+
+
+
+Esse é um projeto para aprendizado de criação de formulário utilizando HTML, CSS, JavaScript e Bootstrap. No código, também foi utilizado flexbox. :) 
+
+
---
+++ script/script.js
@@ -0,0 +1,32 @@
+function mostraValue(name){
+    let rates = document.getElementsByName(name);
+    let rate_value = 0;
+    for(let i = 0; i < rates.length; i++){
+        if(rates[i].checked){
+           rate_value = rate_value + parseInt(rates[i].value);
+           
+        } 
+    }
+    return rate_value
+}
+
+function calculaRespostas(){
+    let count = 0;
+    count = mostraValue('halpern');
+    count = count + mostraValue ('flenderson');
+    count = count + mostraValue ('malone');
+    count = count + mostraValue ('philbin');
+    count = count + mostraValue ('scott');
+    count = count + mostraValue ('martinez');
+    count = count + mostraValue ('beesley');
+    count = count + mostraValue ('hudson');
+    count = count + mostraValue ('kapoor');
+    count = count + mostraValue ('vance');
+    alert("Você acertou: " + count + "/10")
+
+}
+
+
+
+
+
