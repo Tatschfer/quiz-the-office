81d1be1080c5439bf1c67e23eee26962c7455ce3
Tatschfer <fernandaamengual@yahoo.com.br>
2021-12-20 23:50:47 -0300

    ajustes-modais

--- script/script.js
+++ script/script.js
@@ -24,24 +24,126 @@ function calculaRespostas(){
     count = count + mostraValue ('vance');
     
     if (count == 0){
-        (alert("Que decepção! Você acertou: " + imagem + count + "/10"));
+        Swal.fire({
+            title: 'Que decepção!',
+            text: 'Você acertou: ' + count + "/10",
+            imageUrl: 'img/respostas0.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'michael-scott',
+          })
     };
 
     if (count == 1){
-            (alert("Você não vê a série? Você acertou apenas: " + count + "/10"));
+        Swal.fire({
+            title: 'Você não vê a série?',
+            text: 'Você acertou: ' + count + "/10",
+            imageUrl: 'img/respostas1.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'dwight-schrute',
+          })
 
     };
 
     if (count == 2){
-        (alert("Ainda está lamentável seu desempenho. Você acertou: " + count + "/10"));
+        Swal.fire({
+            title: 'Ainda está lamentável.',
+            text: 'Você acertou: ' + count + "/10",
+            imageUrl: 'img/respostas2.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'andy-bernard',
+          })
     };
 
     if (count == 3){
-        (alert("Não está nem perto de ficar bom. Você acertou: " + count + "/10"));
+        Swal.fire({
+            title: 'Não está nem perto de ficar bom.',
+            text: 'Você acertou: ' + count + "/10",
+            imageUrl: 'img/respostas3.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'michael-s',
+          })
     };
 
     if (count == 4){
-        (alert("Opa, será que vai melhorar? Você acertou: " + count + "/10"));
+        Swal.fire({
+            title: 'Ainda não...',
+            text: 'Você acertou: ' + count + "/10",
+            imageUrl: 'img/respostas4.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'pam-not-yet',
+          })
+    };
+
+    if (count == 5){
+        Swal.fire({
+            title: 'Tente de novo.',
+            text: 'Você acertou: ' + count + "/10",
+            imageUrl: 'img/respostas5.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'jim-try-again',
+          })
+    };
+
+    if (count == 6){
+        Swal.fire({
+            title: 'Opa, está começando a melhorar!',
+            text: 'Você acertou: ' + count + "/10",
+            imageUrl: 'img/respostas6.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'phillis',
+          })
+    };
+
+    if (count == 7){
+        Swal.fire({
+            title: 'High five!',
+            text: 'Você acertou: ' + count + "/10",
+            imageUrl: 'img/respostas7.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'high-five',
+          })
+    };
+
+    if (count == 8){
+        Swal.fire({
+            title: 'Está ficando interessante!',
+            text: 'Você acertou: ' + count + "/10",
+            imageUrl: 'img/respostas8.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'expected-michael',
+          })
+    };
+
+    if (count == 9){
+        Swal.fire({
+            title: 'Você é bom nisso!',
+            text: 'Você acertou: ' + count + "/10",
+            imageUrl: 'img/respostas9.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'surprise-michael',
+          })
+    };
+
+    if (count == 10){
+        Swal.fire({
+            title: 'INCRÍVEL!',
+            text: 'Você acertou: ' + count + "/10",
+            imageUrl: 'img/respostas10.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'michael-erin',
+          })
     };
 
 };
+
